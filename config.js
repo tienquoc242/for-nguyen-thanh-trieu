5cffa31399a4dfdd63d9e9b23da52488d00d2e76
tienquoc242 <69025388+tienquoc242@users.noreply.github.com>
2021-08-11 12:25:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Triều Triều à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introDesc: `Trái đất vốn lạ thường, mà sao anh cứ đi nhầm đường. Lạc vào tim em lẻ loi, đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Sến dzậy hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là yêu em <33',
+    btnYes: 'Vẫn cứ là yêu em <33, em là nhứt',
     btnNo: 'Không, em trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Triều mà không dành cho Quốc thì dành cho ai? Yêu em lắm lắm luôn thằng quỷ à',
     mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h em qua đón nhé đứa con ương bướng.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/tienquoc242' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
